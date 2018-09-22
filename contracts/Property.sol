d7ef09de485e63cf7f0d4652884817599a9fca95
Jon Gaspar <jongaspar@gmail.com>
2018-09-22 10:21:56 -0700

    added test for inviteGuest

--- contracts/Property.sol
+++ contracts/Property.sol
@@ -3,9 +3,9 @@ pragma solidity ^0.4.24;
 import "./HelloWorld.sol";
 
 contract Property is HelloWorld {
-    address currentGuest;
+    address public currentGuest;
     
-    function inviteGuest(address _guest) external view onlyOwner returns(address) {
-        return _guest;
+    function inviteGuest(address _guest) external onlyOwner returns(address) {
+        return currentGuest = _guest;
     }
 }
--- test/property.js
+++ test/property.js
@@ -4,9 +4,23 @@ const Property = artifacts.require('./Property.sol')
 // Tests
 contract('Property Contract tests', async accounts => {
   let property
+  const alice = accounts[0];
+  const bob = accounts[0];
 
   it('should be deployed', async () => {
     property = await Property.deployed()
     assert(property !== undefined, 'Property was NOT deployed')
+  })
+
+  it('alice (the owner) can invite bob (the guest)', async () => {
+    property = await Property.deployed()
+
+    try {
+      await property.inviteGuest(bob, { from: alice })
+      const currentGuest = await property.currentGuest.call();
+      assert(currentGuest === bob, 'alice can invite bob')
+    } catch (e) {
+      assert(false, 'alice could not invite bob')
+    }
   })
 })
