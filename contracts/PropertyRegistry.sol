cd0bd0c65aff297df63293c59b0a858fe7664ff5
Jon Gaspar <jongaspar@gmail.com>
2018-10-01 20:39:39 -0700

    implemented new functions, some failing tests tho still

--- contracts/PropertyRegistry.sol
+++ contracts/PropertyRegistry.sol
@@ -5,12 +5,15 @@ import "openzeppelin-solidity/contracts/token/ERC721/ERC721Basic.sol";
 contract PropertyRegistry {
   ERC721Basic property;
 
-  mapping (uint256 => Data) public stayData;
+  mapping (uint => Stay) public stays;
 
-  struct Data {
-    uint256 price;
-    uint256 stays;
+  struct Stay {
+    uint price;
     address occupant;
+    uint checkIn;
+    uint checkOut;
+    bool isApproved;
+    bool isCheckedIn;
   }
 
   // Set up the property contract as minimum interface to prove ownership ERC721Basic
@@ -18,16 +21,35 @@ contract PropertyRegistry {
     property = ERC721Basic(_property);
   }
 
-  modifier onlyOwner(uint256 _tokenId) {
+  modifier onlyOwner(uint _tokenId) {
     require(property.ownerOf(_tokenId) == msg.sender, "You must be the owner");
     _;
   }
 
-  function registerProperty(uint256 _tokenId, uint256 _price) external onlyOwner(_tokenId) {
-    stayData[_tokenId] = Data(_price, 0, address(0));
+  function registerProperty(uint _tokenId, uint _price) external onlyOwner(_tokenId) {
+    stays[_tokenId] = Stay(_price, address(0), 0, 0, false, false);
   }
 
-  function request() external pure returns (uint) {
-    return 5;
+
+  function request(uint _tokenId, uint _checkIn, uint _checkOut) external {
+    stays[_tokenId].checkIn = _checkIn;
+    stays[_tokenId].checkOut = _checkOut;
+    stays[_tokenId].occupant = msg.sender;
+  }
+
+  function approveRequest(uint _tokenId) external onlyOwner(_tokenId) {
+    stays[_tokenId].isApproved = true;
+  }
+
+  function checkIn(uint _tokenId) external {
+    stays[_tokenId].isCheckedIn = true;
+  }
+
+  function checkOut(uint _tokenId) external {
+    stays[_tokenId].checkIn = 0;
+    stays[_tokenId].checkOut = 0;
+    stays[_tokenId].occupant = address(0);
+    stays[_tokenId].isApproved = false;
+    stays[_tokenId].isCheckedIn = false;
   }
 }
