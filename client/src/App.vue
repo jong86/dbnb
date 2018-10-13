94f55ce444864e67270f10d4b6bc7105866c0941
Jon Gaspar <jongaspar@gmail.com>
2018-10-12 20:54:09 -0700

    added web3 init stuff

--- client/src/App.vue
+++ client/src/App.vue
@@ -7,6 +7,18 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Web3 from 'web3'
+
+let web3Provider
+
+if (typeof web3 !== "undefined") {
+  web3Provider = web3.currentProvider
+} else {
+  web3Provider = new Web3.providers.HttpProvider('http://127.0.0.1:9545')
+}
+
+window.web3 = new Web3(web3Provider)
+
 
 export default {
   name: 'app',
