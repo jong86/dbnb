4cee303af3fe202bfcb3be413b1b833861af3bde
Jon Gaspar <jongaspar@gmail.com>
2018-10-16 21:11:01 -0700

    added seedState script

---
+++ scripts/seedState.js
@@ -0,0 +1,16 @@
+const Property = artifacts.require('./Property.sol')
+const PropertyToken = artifacts.require('./PropertyToken.sol')
+const PropertyRegistry = artifacts.require('./PropertyRegistry.sol')
+
+module.exports = function(done) {
+
+  const owner = "0x627306090abab3a6e1400e9345bc60c78a8bef57"
+  const bob = "0xf17f52151ebef6c7334fad080c5704d77216b732"
+  const alice = "0xc5fdf4076b8f3a5357c5e395ab970b5b54098fef"
+
+
+
+  
+
+  done()
+}
